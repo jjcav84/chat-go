ecf780f6e373349b2fd871b83cf1d77f85623b9e
Jacob Cavazos <jjcav84@gmail.com>
2019-03-31 12:21:46 -0500

    git http server working with response from template at localhost:8080

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
 // templ represents a single template
@@ -15,10 +18,9 @@ type templateHandler struct {
 // ServeHTTP handles the HTTP request.
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
-			t.filename)))
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
