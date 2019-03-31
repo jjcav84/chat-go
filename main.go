9f172b045de6f32f27db44bdeec2abd9cbeec29a
Jacob Cavazos <jjcav84@gmail.com>
2019-03-31 11:57:27 -0500

    added chat.html to template directory and updated main.go with templateHandler struct and ServeHTTP for the server

--- main.go
+++ main.go
@@ -5,6 +5,22 @@ import (
 	"net/http"
 )
 
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
