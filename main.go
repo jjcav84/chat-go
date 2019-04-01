21ea07b958cb49782c68501a396f9262255cb2ec
Jacob Cavazos <jjcav84@gmail.com>
2019-04-01 03:31:55 -0500

    avatars have been implemented properly

--- main.go
+++ main.go
@@ -2,48 +2,56 @@ package main
 
 import (
 	"flag"
-	"github.com/matryer/goblueprints/chapter1/trace"
-	"github.com/stretchr/gomniauth"
-	"github.com/stretchr/gomniauth/providers/github"
-	"github.com/stretchr/objx"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-	"sync"
 	"text/template"
+
+	"github.com/matryer/goblueprints/chapter1/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/objx"
 )
+
+// set the active Avatar implementation
+var avatars Avatar = TryAvatars{
+	UseFileSystemAvatar,
+	UseAuthAvatar,
+	UseGravatar}
 
 // templ represents a single template
 type templateHandler struct {
-	once     sync.Once
 	filename string
 	templ    *template.Template
 }
 
 // ServeHTTP handles the HTTP request.
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
-			t.filename)))
-	})
+	if t.templ == nil {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	}
+
 	data := map[string]interface{}{
 		"Host": r.Host,
 	}
 	if authCookie, err := r.Cookie("auth"); err == nil {
 		data["UserData"] = objx.MustFromBase64(authCookie.Value)
 	}
+
 	t.templ.Execute(w, data)
 }
 
+var host = flag.String("host", ":8080", "The host of the application.")
+
 func main() {
-	var addr = flag.String("addr", ":8080", "The addr of the application.")
+
 	flag.Parse() // parse the flags
+
 	// setup gomniauth
 	gomniauth.SetSecurityKey("PUT YOUR AUTH KEY HERE")
 	gomniauth.WithProviders(
-		github.New("23019f2f3dfeebeb5bb1", "7b20e351c2de059f04785a152c01843294e646ea",
-			"http://localhost:8080/auth/callback/github"),
+		github.New("23019f2f3dfeebeb5bb1", "7b20e351c2de059f04785a152c01843294e646ea", "http://localhost:8080/auth/callback/github"),
 	)
 
 	r := newRoom()
@@ -53,13 +61,29 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+	http.Handle("/upload", &templateHandler{filename: "upload.html"})
+	http.HandleFunc("/uploader", uploaderHandler)
+
+	http.Handle("/avatars/",
+		http.StripPrefix("/avatars/",
+			http.FileServer(http.Dir("./avatars"))))
 
 	// get the room going
 	go r.run()
 
 	// start the web server
-	log.Println("Starting web server on", *addr)
-	if err := http.ListenAndServe(*addr, nil); err != nil {
+	log.Println("Starting web server on", *host)
+	if err := http.ListenAndServe(*host, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 
